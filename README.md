f48dc43394b5a09b32b63aedf3fe6abbb35df2f5
matthew-deluge <matthewj913@gmail.com>
2020-11-11 13:47:30 -0500

    refactored using rubocop

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # mastermind
+
+This is a command line version of the classic game "Mastermind"
+
+It has two modes:
+1. You can play as the "code breaker", and the computer makes the code.
+2. You can play as the "code master" and make the code for the computer.
+
+The project was created for The Odin Project curriculum, and demonstrates:
+- Understanding and use of classes and inheritance
+- Understanding and use of methods
+- Understanding and use of enumeration methods
+
--- mastermind.rb
+++ mastermind.rb
@@ -1,42 +1,73 @@
 # mastermind
-require "pry"
+require 'pry'
+# allows for colored strings: https://stackoverflow.com/questions/1489183/colorized-ruby-output-to-the-terminal
+class String
+  # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
 
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def yellow
+    colorize(33)
+  end
+
+  def blue
+    colorize(34)
+  end
+
+  def pink
+    colorize(35)
+  end
+
+  def light_blue
+    colorize(36)
+  end
+end
+
+# the main game class, allows for player-guessing, computer-creating games
 class Mastermind
-  attr_reader :game_board
-  attr_reader :result_board
+  attr_reader :game_board, :result_board
 
   def initialize
     @game_board = []
     @result_board = []
     @player_input = []
     @turn = 0
-    @colors = ["r","b","y","g"]
+    @colors = %w[r b y g]
     intro
     make_board
   end
 
   def intro
     puts "It's time for a game of"
-    "Mastermind!\n".split("").each do |letter| 
+    'Mastermind!'.split('').each do |letter|
       print letter
       sleep 0.2
-    end 
+    end
   end
 
   def make_board
-    for a in 1..4 do
+    for i in 1..4 do
       @game_board.push(@colors.sample)
     end
   end
 
-  def get_input
-    puts "what is your guess?"
-    @player_input = gets.chomp.split("")
+  def prompt_input
+    puts "\nwhat is your guess?"
+    @player_input = gets.chomp.split('')
     until @player_input.length == 4 && (@player_input - @colors == [])
       puts "Your guess must be four letter long, and made up of r, b, y, and g.\n Try again!"
-      @player_input = gets.chomp.split("")
+      @player_input = gets.chomp.split('')
     end
-    puts "you guessed #{@player_input}"
+    puts "\nYou guessed #{@player_input.join(' ')}"
   end
 
   def create_result(input_guess)
@@ -50,9 +81,9 @@ class Mastermind
         temp_input[index] = "X"
       end
     end
-    temp_input.each_with_index do |choice, index|
-      for i in temp_board do
-        if i == choice
+    temp_input.each do |choice|
+      temp_board.each do |letter|
+        if letter == choice
           @result_board.push("w")
           break
         end
@@ -62,7 +93,7 @@ class Mastermind
   end
 
   def check_win
-    return result_board == ["b","b","b","b"] 
+    result_board == ['b', 'b', 'b', 'b'] 
   end
 
   def new_turn
@@ -71,75 +102,83 @@ class Mastermind
 
   def play
     until @turn == 12 || check_win
-      puts "there are #{@turn} turns left."
-      get_input
-      create_result (@player_input)
-      puts @result_board
+      puts "there are #{12 - @turn} turns left.\n"
+      prompt_input
+      create_result(@player_input)
+      puts "You see #{@result_board.join(' ')}\n\n"
       new_turn
       @turn += 1
     end
-    result = check_win ? "You won! congrats!" : "You lost, poor you!"
+    result = check_win ? 'You won! congrats!' : 'You lost, poor you!'
     puts result
   end
 end
 
+# Version of Mastermind where the computer guesses the code
 class Computermind < Mastermind
-  
   def initialize
     super
     @solutions = @colors.repeated_permutation(4).to_a
   end
 
   def make_board
-    puts "What is your secret code? Remember your code can include b, g, r, y, and must be four letters long!"
-    @game_board = gets.chomp.split("")
+    puts 'What is your secret code? Remember your code can include b, g, r, y, and must be four letters long!'
+    @game_board = gets.chomp.split('')
     until @game_board.length == 4 && (@game_board - @colors == [])
-      binding.pry
       puts "Your code must be four letter long, and made up of r, b, y, and g.\n Try again!"
-      @game_board = gets.chomp.split("")
+      @game_board = gets.chomp.split('')
     end
   end
 
   def computer_guess
-    if @turn == 0
-      for a in 1..4 do
-        @player_input.push(@colors.sample)
-      end
-    else 
-      @solutions.delete_if {|solution| create_result(solution) == create_result(@player_input)}
+    if @turn.zero?
+      @player_input = %w[r r y y]
+    else
+      @solutions.delete_if { |solution| create_result(solution) == create_result(@player_input) }
       @player_input = @solutions[0]
     end
-    
   end
 
   def play
-      until @turn == 12 || check_win
-        puts "there are #{12 - @turn} turns left."
-        computer_guess
-        puts "The computer guessed #{@player_input}, and your code is #{@game_board}"
-        create_result(@player_input)
-        puts "The computer sees: #{@result_board}"
-        sleep 0.2
-        @turn += 1
+    until @turn == 12 || check_win
+      puts "there are #{12 - @turn} turns left."
+      computer_guess
+      puts "The computer guessed #{@player_input.join(' ')}, and your code is #{@game_board.join(' ')}"
+      create_result(@player_input)
+      puts "The computer sees: #{@result_board.join(' ')}"
+      print 'calculating'
+      for i in 1..10 do
+        print '.'
+        sleep 0.1
       end
-      result = check_win ? "The computer won! The apocalypse is here!!" : "The computer lost! The singularity is avoided!"
-      puts result
+      print "\n"
+      @turn += 1
+    end
+    result = check_win ? 'The computer won! The apocalypse is here!!' : 'The computer lost! The singularity is avoided!'
+    puts result
   end
-
 end
 
-puts "Welcome to Mastermind! You have two options:\nTo break the code, input '1'\nTo make the code, input'2'"
+puts "Welcome to Mastermind!\n
+in this game, one playe comes up with a code using\n\n
+#{'red'.red}, #{'green'.green}, #{'blue'.blue}, and #{'yellow'.yellow} pegs\n
+Then, the other player guesses the code!\n
+If they guessed a correct color in a correct spot, then they see a black ('b') peg\n
+If they guessed a correct color in an incorrect spot, they see a white ('w') peg\n
+They then get to guess again based on this information!\n
+This repeats for twelve turns; if the guesser gets the code in that time, they win!"
+
+puts "You have two options:\nTo break the code, input '1'\nTo make the code, input'2'"
 input = gets.chomp
-until input == "1"||input == "2"
+until ['1','2'].include?(input)
   puts "You have two options:\nTo break the code, input '1'\nTo make the code, input'2'"
   input = gets.chomp
 end
 case input
-when "1"
+when '1'
   game = Mastermind.new
   game.play
-when "2"
+when '2'
   game = Computermind.new
   game.play
 end
-
